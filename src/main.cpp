e6e6b375317f7f9f8e652f7dca9a17f46e81cd04
5mil <5mil@users.noreply.github.com>
2016-05-11 17:04:21 -0400

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1075,8 +1075,8 @@ int64 static GetBlockValue(int nHeight, int64 nFees)
 {
     int64 nSubsidy = 0.5 * COIN;
  {
- if nHeight = 2
- nSubsidy = 50000
+ if (nHeight = 2)
+ then (nSubsidy = 50000)
  }
  
     // Subsidy is cut in half every 840000 blocks, which will occur approximately every 4 years
