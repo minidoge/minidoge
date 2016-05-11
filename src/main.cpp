79c7f9025a6bfcf50395c7ac33f7f5ce608587e5
5mil <5mil@users.noreply.github.com>
2016-05-11 16:07:59 -0400

    Update main.cpp
    development quadrant

--- src/main.cpp
+++ src/main.cpp
@@ -1074,7 +1074,11 @@ uint256 static GetOrphanRoot(const CBlockHeader* pblock)
 int64 static GetBlockValue(int nHeight, int64 nFees)
 {
     int64 nSubsidy = 0.5 * COIN;
-
+ {
+ if nHeight = 2
+ nSubsidy = 50000
+ }
+ 
     // Subsidy is cut in half every 840000 blocks, which will occur approximately every 4 years
     nSubsidy >>= (nHeight / 840000); // minidoge: 840k blocks in ~4 years
 
